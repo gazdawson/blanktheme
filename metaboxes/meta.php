cb5fd8746df6468eb99e53b5d8a3404fd03dab58
gazdawson <gazdawson@me.com>
2013-11-28 15:53:54 +0000

    init

---
+++ metaboxes/meta.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Include and setup custom metaboxes and fields.
+ *
+ * @category YourThemeOrPlugin
+ * @package  Metaboxes
+ * @license  http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
+ * @link     https://github.com/jaredatch/Custom-Metaboxes-and-Fields-for-WordPress
+ */
+
+add_filter( 'cmb_meta_boxes', 'krank_metaboxes' );
+/**
+ * Define the metabox and field configurations.
+ *
+ * @param  array $meta_boxes
+ * @return array
+ */
+function krank_metaboxes( array $meta_boxes ) {
+
+	// Start with an underscore to hide fields from custom fields list
+	$prefix = 'krank_';
+
+	$meta_boxes[] = array(
+		'id'         => 'page_design',
+		'title'      => 'Page Design Options',
+		'pages'      => array( 'page', ), // Post type
+		'context'    => 'side',
+		'priority'   => 'low',
+		'show_names' => true, // Show field names on the left
+		'fields'     => array(
+			array(
+				'name' => 'Remove Container',
+				'desc' => 'Check if you wish the page container to be removed from this page.',
+				'id'   => $prefix . 'container',
+				'type' => 'checkbox',
+			),
+		),
+	);
+	
+	// Add other metaboxes as needed
+
+	return $meta_boxes;
+}
+
+add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 9999 );
+/**
+ * Initialize the metabox class.
+ */
+function cmb_initialize_cmb_meta_boxes() {
+
+	if ( ! class_exists( 'cmb_Meta_Box' ) )
+		require_once 'init.php';
+
+}
---
+++ templates/page-header.php
@@ -0,0 +1,5 @@
+<div class="page-header">
+  <h1>
+    <?php echo krank_title(); ?>
+  </h1>
+</div>
