90bbeaa283712196bda3ca3bd5462550021c1723
gazdawson <gazdawson@me.com>
2013-11-29 13:49:58 +0000

    Seperated address and contact shortcodes

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -12,10 +12,6 @@ function krank_address( $atts ) {
        'title' => '',
    ), $atts));
    
-   if($krank['mob'] != ""):
-	   $mob = '<abbr title="Mobile">M: </abbr><a class="mob" itemprop="telephone" href="tel:'.$krank['mob'].'" title="Call ' .$krank['name'].' Today">'.$krank['mob'].'</a>';
-   endif;
-   
    $address = 
    	'<address class="vcard" itemtype="http://schema.org/LocalBusiness" itemscope="">
    		<a class="org" href="'.site_url().'"><span itemprop="name">'.$krank['name'].'</span></a>
@@ -24,17 +20,36 @@ function krank_address( $atts ) {
 	   		<span class="locality" itemprop="addressLocality">'.$krank['town'].'</span>
 	   		<span class="region" itemprop="addressRegion">'.$krank['region'].'</span>
 	   		<span class="postal-code" itemprop="postalCode">'.$krank['postcode'].'</span>
-		
-	    	<abbr title="Landline">T: </abbr><a class="tel" itemprop="telephone" href="tel:'.$krank['tel'].'" title="Call '. $krank['name'].' Today">'.$krank['tel'].'</a>'
-	    	.$mob.
-	    	'<abbr title="Website">W: </abbr><a class="url" itemprop="url" href="'.site_url().'">'.site_url().'</a>
-	    	<abbr title="Email">E: </abbr><a class="email" href="'.$krank['email'].'" title="Get In Contact With '.$krank['name'].' Today"><meta itemprop="email" content="'.$krank['email'].'">'.$krank['email'].'</a>
 		</span>
    	</address><!--/address-->';
 	
 	return $address;
 }
 add_shortcode('address', 'krank_address');
+
+//[contact title="title"]
+function krank_contact( $atts ) {
+	global $krank;
+	
+   extract(shortcode_atts(array(
+       'title' => '',
+   ), $atts));
+   
+	if($krank['mob'] != ""):
+	   $mob = '<abbr title="Mobile">M: </abbr><a class="mob" itemprop="telephone" href="tel:'.$krank['mob'].'" title="Call ' .$krank['name'].' Today">'.$krank['mob'].'</a>';
+	endif;
+
+	$contact =
+	'<ul class="contact" itemtype="http://schema.org/LocalBusiness" itemscope="">
+		<li><abbr title="Landline">T: </abbr><a class="tel" itemprop="telephone" href="tel:'.$krank['tel'].'" title="Call '. $krank['name'].' Today">'.$krank['tel'].'</a></li>'.
+		'<li>'.$mob.'</li>'.
+		'<li><abbr title="Website">W: </abbr><a class="url" itemprop="url" href="'.site_url().'">'.site_url().'</a></li>
+		<li><abbr title="Email">E: </abbr><a class="email" href="'.$krank['email'].'" title="Get In Contact With '.$krank['name'].' Today"><meta itemprop="email" content="'.$krank['email'].'">'.$krank['email'].'</a></li>
+	</ul>';
+
+	return $contact;
+}
+add_shortcode('contact', 'krank_contact');
 
 //[open-hours title="title"]
 function krank_opening( $atts ) {
