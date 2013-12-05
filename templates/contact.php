5cf68126e6b5666c60eca684db8b89aa54d8e761
gazdawson <gazdawson@me.com>
2013-12-05 13:31:38 +0000

    Fixed Shortcode Issues

--- templates/contact.php
+++ templates/contact.php
@@ -11,21 +11,21 @@ global $krank;
 <?php get_template_part('templates/content', 'page'); // Page Content?>
 
 <div class="contact-info row">
+	<div class="business-info">
+		<?php echo do_shortcode('[address title=""]'); ?>
+		<?php echo do_shortcode('[open-hours title=""]'); ?>
+		<?php echo do_shortcode('[contact title=""]'); ?>
+	</div>
 	<div class="contact-form">
 		<h3 class="contact-title">Send Us a Message</h3>
 		<?php echo do_shortcode('[contact-form-7 id="' . $krank['contact_7'] . '"]')?>
 	</div><!--/.contact-form-->
-	<div class="business-info">
+	<div class="map">
 		<h3 class="contact-title">Where We Are</h3>
 		<div id="map" class="google-map"></div>
-		<h3 class="contact-title">Address</h3>
-		<?php echo krank_address(); ?>
-		<h3 class="contact-title">Contact Details</h3>
-		<?php echo krank_contact(); ?>
-		<h3 class="contact-title">Opening Hours</h3>
-		<?php echo krank_opening(); ?>
 	</div><!--/.business-info-->
 </div>
+
 <?php foreach ($krank['page_sidebar'] as $page):
 		$side_pages .= $page.',';
 	endforeach;
--- templates/footer.php
+++ templates/footer.php
@@ -3,9 +3,9 @@
 <footer class="content-info container" role="contentinfo">
   <div class="row">
     <div class="col-lg-12">
-	  <?php echo krank_address(); ?>
-	  <?php echo krank_contact(); ?>
-	  <?php echo krank_opening(); ?>
+	<?php echo do_shortcode('[address title=""]'); ?>
+	<?php echo do_shortcode('[open-hours title=""]'); ?>
+	<?php echo do_shortcode('[contact title=""]'); ?>
       <?php
         if (has_nav_menu('footer_navigation')) :
           wp_nav_menu(array('theme_location' => 'footer_navigation', 'menu_class' => 'nav'));
@@ -31,7 +31,7 @@
       </div>
       <div class="modal-footer">
 		<p>Alternatively you can use any of our other forms of contact to get in touch.</p>
-		<?php echo krank_contact(); ?>
+		<?php echo do_shortcode('[contact title=""]'); ?>
         <button type="button" class="btn btn-default" data-dismiss="modal" aria-hidden="true">Close</button>
       </div>
     </div><!-- /.modal-content -->
