7381883076ce56ba3550f37aea97a048ba8e9e58
gazdawson <gazdawson@me.com>
2013-12-05 09:58:32 +0000

    Added gallery caption to gallery plugin

--- lib/gallery.php
+++ lib/gallery.php
@@ -7,7 +7,7 @@
  *
  * @link http://getbootstrap.com/components/#thumbnails
  */
-function roots_gallery($attr) {
+function krank_gallery($attr) {
   $post = get_post();
 
   static $instance = 0;
@@ -38,8 +38,8 @@ function roots_gallery($attr) {
     'orderby'    => 'menu_order ID',
     'id'         => $post->ID,
     'itemtag'    => '',
-    'icontag'    => '',
-    'captiontag' => '',
+    'icontag'    => 'li',
+    'captiontag' => 'p',
     'columns'    => 4,
     'size'       => 'thumbnail',
     'include'    => '',
@@ -90,7 +90,7 @@ function roots_gallery($attr) {
     $output .= '<div class="' . $grid .'">' . $image;
 
     if (trim($attachment->post_excerpt)) {
-      $output .= '<div class="caption hidden">' . wptexturize($attachment->post_excerpt) . '</div>';
+      $output .= '<div class="caption">' . wptexturize($attachment->post_excerpt) . '</div>'; //remove hidden class to display caption
     }
 
     $output .= '</div>';
@@ -105,16 +105,16 @@ function roots_gallery($attr) {
 }
 if (current_theme_supports('bootstrap-gallery')) {
   remove_shortcode('gallery');
-  add_shortcode('gallery', 'roots_gallery');
+  add_shortcode('gallery', 'krank_gallery');
   add_filter('use_default_gallery_style', '__return_null');
 }
 
 /**
  * Add class="thumbnail img-thumbnail" to attachment items
  */
-function roots_attachment_link_class($html) {
+function krank_attachment_link_class($html) {
   $postid = get_the_ID();
   $html = str_replace('<a', '<a class="thumbnail img-thumbnail"', $html);
   return $html;
 }
-add_filter('wp_get_attachment_link', 'roots_attachment_link_class', 10, 1);
+add_filter('wp_get_attachment_link', 'krank_attachment_link_class', 10, 1);
