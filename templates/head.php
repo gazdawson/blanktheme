ec73876dd8608d8ca990bb81a0c2240386198618
gazdawson <gazdawson@me.com>
2013-12-17 16:58:16 +0000

    Corrected Page Title Tag.

--- templates/head.php
+++ templates/head.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?php wp_title('|', true, 'right'); ?></title>
+   <title><?php wp_title('|', true, 'right'); ?> | <?php bloginfo('description'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <?php wp_head(); ?>
