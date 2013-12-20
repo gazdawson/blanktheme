820b2bd6e76fa3375424edcfb79c1ea5f0993e52
gazdawson <gazdawson@me.com>
2013-12-20 14:42:20 +0000

    css changes

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -73,3 +73,103 @@ function krank_opening( $atts ) {
 		.'</ul><!--/.opening-hours-->';
 }
 add_shortcode('open-hours', 'krank_opening');
+
+//[section title="title"]
+function krank_section( $atts, $content = null ) {
+
+	extract(shortcode_atts(array(
+		'title' => '',
+	), $atts));
+	
+	$output = 
+		'<section class="section">
+			<h3 class="section-title">'.$title.'</h3>'.
+			do_shortcode( $content ).
+		'</section>';
+	
+	return $output;
+}
+add_shortcode('section', 'krank_section');
+
+//[lead]
+function krank_lead( $atts, $content = null ) {
+	$output = 
+		'<p class="lead">'.
+			do_shortcode( $content ).
+		'</p>';
+	
+	return $output;
+}
+add_shortcode('lead', 'krank_lead');
+
+//[column md="6"]
+function krank_column( $atts, $content = null ) {
+  extract(shortcode_atts(array(
+    "lg" => false,
+    "md" => false,
+    "sm" => false,
+    "xs" => false,
+    "offset_lg" => false,
+    "offset_md" => false,
+    "offset_sm" => false,
+    "offset_xs" => false,
+    "pull_lg" => false,
+    "pull_md" => false,
+    "pull_sm" => false,
+    "pull_xs" => false,
+    "push_lg" => false,
+    "push_md" => false,
+    "push_sm" => false,
+    "push_xs" => false,
+  ), $atts));
+  $return  =  '<div class="';
+  $return .= ($lg) ? 'col-lg-' . $lg . ' ' : '';
+  $return .= ($md) ? 'col-md-' . $md . ' ' : '';
+  $return .= ($sm) ? 'col-sm-' . $sm . ' ' : '';
+  $return .= ($xs) ? 'col-xs-' . $xs . ' ' : '';
+  $return .= ($offset_lg) ? 'col-lg-offset-' . $offset_lg . ' ' : '';
+  $return .= ($offset_md) ? 'col-md-offset-' . $offset_md . ' ' : '';
+  $return .= ($offset_sm) ? 'col-sm-offset-' . $offset_sm . ' ' : '';
+  $return .= ($offset_xs) ? 'col-xs-offset-' . $offset_xs . ' ' : '';
+  $return .= ($pull_lg) ? 'col-lg-pull-' . $pull_lg . ' ' : '';
+  $return .= ($pull_md) ? 'col-md-pull-' . $pull_md . ' ' : '';
+  $return .= ($pull_sm) ? 'col-sm-pull-' . $pull_sm . ' ' : '';
+  $return .= ($pull_xs) ? 'col-xs-pull-' . $pull_xs . ' ' : '';
+  $return .= ($push_lg) ? 'col-lg-push-' . $push_lg . ' ' : '';
+  $return .= ($push_md) ? 'col-md-push-' . $push_md . ' ' : '';
+  $return .= ($push_sm) ? 'col-sm-push-' . $push_sm . ' ' : '';
+  $return .= ($push_xs) ? 'col-xs-push-' . $push_xs . ' ' : '';
+  $return .= '">' . do_shortcode( $content ) . '</div>';
+
+  return $return;
+}
+add_shortcode('column', 'krank_column');
+
+//[element class=""]
+function krank_element( $atts, $content = null ) {
+	
+	extract(shortcode_atts(array(
+		'class' => '',
+	), $atts));
+	
+	$output = 
+		'<div class="'.$class.'">'.
+			do_shortcode( $content ).
+		'</div>';
+	
+	return $output;
+}
+add_shortcode('element', 'krank_element');
+
+//[fa-icon icon=""]
+function krank_icon( $atts ) {
+	
+	extract(shortcode_atts(array(
+		'icon' => '',
+	), $atts));
+	
+	$output = '<i class="fa fa-'.$icon.'"></i>';
+	
+	return $output;
+}
+add_shortcode('fa-icon', 'krank_icon');
